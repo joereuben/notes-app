00fe96fc2adffc114dc6693ec098e25b96f77473
Joseph <jreuben414@gmail.com>
2022-07-17 15:24:44 +0000

    Notes with text truncated and button to show more

--- src/Note.js
+++ src/Note.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Note({note, deleteNote}) {
+
+    const [showMore, setShowMore] = useState(false)
+    const {id, text} = note
+
   return (
     <div className='note_box'>
       <div>
-        <span className='note_text'>{note.text}</span>
+        <span className='note_text'>
+            {/* If text is 50 or less characters, show the text without any truncation,
+             other wise display with possible truncation(depending on value of showMore) */}
+            {(text.length <= 50) ? text :
+            showMore ? text : `${text.substring(0,50)}...`}
+
+            {/* If text is more than 50 characters, display the "show more" button */}
+            {(text.length > 50) && <button className='note_text_toggle_btn' onClick={() => setShowMore(!showMore)}>
+                {showMore ? "Show Less" : "Show More"}</button>}
+            
+        </span>
         
       </div>
       <div className='delete_box'>
-        <button type='button' className='delete_btn button' onClick={()=> deleteNote(note.id)}>Delete</button>
+        <button type='button' className='delete_btn button' onClick={()=> deleteNote(id)}>Delete</button>
       </div>
     </div>
   )
--- src/NoteArea.js
+++ src/NoteArea.js
@@ -12,13 +12,20 @@ export default function NoteArea({createNote}) {
         createNote(note)
         inputRef.current.value = ""
     }
+
+    function onInput(e){
+        e.target.parentNode.dataset.replicatedValue = e.target.value
+    }
     
   return (
     <section>
         <h4>Create Your Note</h4>
         <form action='' method='get' onSubmit={handleSubmit}>
+            <div className='grow-wrap'>
+                <textarea type="text" ref={inputRef} onChange={(e) => setNote(e.target.value)}
+                onInput={onInput}/>
+            </div>
             
-            <textarea type="text" ref={inputRef} onChange={(e) => setNote(e.target.value)}/>
             <div>
                 <button type='submit' className='button'>Save</button>
             </div>
